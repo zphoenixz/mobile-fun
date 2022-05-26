e4c9d7ef68f36743738ae717dfae6cfa514fdf03
zphoenixz <rora.vvaa@gmail.com>
2022-05-26 19:18:44 -0400

    constants

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,7 +1,12 @@
+import 'dart:ui';
+
 class Constants {
   static const double titleFont = 22;
   static const double subtitleFont = 20;
   static const double bodyFont = 18;
 
   static const String mainFont = 'Quicksand';
+
+  static const Color appBarColor = Color(0xff80E1BC);
+  static const Color mediumBlackColor = Color(0xff454545);
 }
